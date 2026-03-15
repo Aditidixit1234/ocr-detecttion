f892a63d70221d3df740f777f7c84b504fe7b31c
Esh@n123 <saxena.eshan@gmail.com>
2026-03-15 13:42:11 +0530

    nlp2

--- nlp.py
+++ nlp.py
@@ -1,22 +1,41 @@
 import re
 import spacy
-from symspellpy import SymSpell
+from symspellpy import SymSpell, Verbosity
 
+# OCR output
 ocr_text = "Meetng nots: Discusd projct deadlns and datset of 200 ims"
 
-clean_text = re.sub(r'[^a-zA-Z0-9\s]', '', ocr_text).lower()
+# Step 1 clean text
+clean_text = re.sub(r'[^a-zA-Z0-9\s]', '', ocr_text)
 
+# Step 2 tokenize
+nlp = spacy.load("en_core_web_sm")
+doc = nlp(clean_text)
+
+tokens = [token.text for token in doc]
+
+# Step 3 initialize symspell
 sym_spell = SymSpell(max_dictionary_edit_distance=2)
 
 sym_spell.load_dictionary(
     "frequency_dictionary_en_82_765.txt",
-    0,
-    1
+    term_index=0,
+    count_index=1
 )
 
-suggestions = sym_spell.lookup_compound(clean_text, max_edit_distance=2)
+# Step 4 correct words
+corrected_words = []
 
-corrected_text = suggestions[0].term
+for word in tokens:
+    suggestions = sym_spell.lookup(word, Verbosity.CLOSEST, max_edit_distance=2)
+
+    if suggestions:
+        corrected_words.append(suggestions[0].term)
+    else:
+        corrected_words.append(word)
+
+# Step 5 rebuild sentence
+final_text = " ".join(corrected_words)
 
 print("Corrected text:")
-print(corrected_text)
+print(final_text)
